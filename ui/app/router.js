594ab4dbd614f0af53f65a70f307201028417e68
Shamil Ishraq <shamil.ishraq@gmail.com>
2017-03-04 14:13:47 -0500

    init ui

---
+++ ui/app/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route("wake-up-call", { path: '/'})
+});
+
+export default Router;
